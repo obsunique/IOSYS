b4f3ca9850a48b0ad41bc4a57789dbf491a55621
caicia <42534127+caicia@users.noreply.github.com>
2018-12-18 10:16:54 +0800

    Update public.js

--- src/main/resources/static/js/public.js
+++ src/main/resources/static/js/public.js
@@ -1,54 +1,54 @@
-<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="jquery.min.js"></script>
 
-<script type="text/javascript" src="js/plugins/spinner/ui.spinner.js"></script>
-<script type="text/javascript" src="js/plugins/spinner/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="plugins/spinner/ui.spinner.js"></script>
+<script type="text/javascript" src="plugins/spinner/jquery.mousewheel.js"></script>
 
-<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="jquery-ui.min.js"></script>
 
-<script type="text/javascript" src="js/plugins/charts/excanvas.min.js"></script>
-<script type="text/javascript" src="js/plugins/charts/jquery.flot.js"></script>
-<script type="text/javascript" src="js/plugins/charts/jquery.flot.orderBars.js"></script>
-<script type="text/javascript" src="js/plugins/charts/jquery.flot.pie.js"></script>
-<script type="text/javascript" src="js/plugins/charts/jquery.flot.resize.js"></script>
-<script type="text/javascript" src="js/plugins/charts/jquery.sparkline.min.js"></script>
+<script type="text/javascript" src="plugins/charts/excanvas.min.js"></script>
+<script type="text/javascript" src="plugins/charts/jquery.flot.js"></script>
+<script type="text/javascript" src="plugins/charts/jquery.flot.orderBars.js"></script>
+<script type="text/javascript" src="plugins/charts/jquery.flot.pie.js"></script>
+<script type="text/javascript" src="plugins/charts/jquery.flot.resize.js"></script>
+<script type="text/javascript" src="plugins/charts/jquery.sparkline.min.js"></script>
 
-<script type="text/javascript" src="js/plugins/forms/uniform.js"></script>
-<script type="text/javascript" src="js/plugins/forms/jquery.cleditor.js"></script>
-<script type="text/javascript" src="js/plugins/forms/jquery.validationEngine-en.js"></script>
-<script type="text/javascript" src="js/plugins/forms/jquery.validationEngine.js"></script>
-<script type="text/javascript" src="js/plugins/forms/jquery.tagsinput.min.js"></script>
-<script type="text/javascript" src="js/plugins/forms/autogrowtextarea.js"></script>
-<script type="text/javascript" src="js/plugins/forms/jquery.maskedinput.min.js"></script>
-<script type="text/javascript" src="js/plugins/forms/jquery.dualListBox.js"></script>
-<script type="text/javascript" src="js/plugins/forms/jquery.inputlimiter.min.js"></script>
-<script type="text/javascript" src="js/plugins/forms/chosen.jquery.min.js"></script>
+<script type="text/javascript" src="plugins/forms/uniform.js"></script>
+<script type="text/javascript" src="plugins/forms/jquery.cleditor.js"></script>
+<script type="text/javascript" src="plugins/forms/jquery.validationEngine-en.js"></script>
+<script type="text/javascript" src="plugins/forms/jquery.validationEngine.js"></script>
+<script type="text/javascript" src="plugins/forms/jquery.tagsinput.min.js"></script>
+<script type="text/javascript" src="plugins/forms/autogrowtextarea.js"></script>
+<script type="text/javascript" src="plugins/forms/jquery.maskedinput.min.js"></script>
+<script type="text/javascript" src="plugins/forms/jquery.dualListBox.js"></script>
+<script type="text/javascript" src="plugins/forms/jquery.inputlimiter.min.js"></script>
+<script type="text/javascript" src="plugins/forms/chosen.jquery.min.js"></script>
 
-<script type="text/javascript" src="js/plugins/wizard/jquery.form.js"></script>
-<script type="text/javascript" src="js/plugins/wizard/jquery.validate.min.js"></script>
-<script type="text/javascript" src="js/plugins/wizard/jquery.form.wizard.js"></script>
+<script type="text/javascript" src="plugins/wizard/jquery.form.js"></script>
+<script type="text/javascript" src="plugins/wizard/jquery.validate.min.js"></script>
+<script type="text/javascript" src="plugins/wizard/jquery.form.wizard.js"></script>
 
-<script type="text/javascript" src="js/plugins/uploader/plupload.js"></script>
-<script type="text/javascript" src="js/plugins/uploader/plupload.html5.js"></script>
-<script type="text/javascript" src="js/plugins/uploader/plupload.html4.js"></script>
-<script type="text/javascript" src="js/plugins/uploader/jquery.plupload.queue.js"></script>
+<script type="text/javascript" src="plugins/uploader/plupload.js"></script>
+<script type="text/javascript" src="plugins/uploader/plupload.html5.js"></script>
+<script type="text/javascript" src="plugins/uploader/plupload.html4.js"></script>
+<script type="text/javascript" src="plugins/uploader/jquery.plupload.queue.js"></script>
 
-<script type="text/javascript" src="js/plugins/tables/datatable.js"></script>
-<script type="text/javascript" src="js/plugins/tables/tablesort.min.js"></script>
-<script type="text/javascript" src="js/plugins/tables/resizable.min.js"></script>
+<script type="text/javascript" src="plugins/tables/datatable.js"></script>
+<script type="text/javascript" src="plugins/tables/tablesort.min.js"></script>
+<script type="text/javascript" src="plugins/tables/resizable.min.js"></script>
 
-<script type="text/javascript" src="js/plugins/ui/jquery.tipsy.js"></script>
-<script type="text/javascript" src="js/plugins/ui/jquery.collapsible.min.js"></script>
-<script type="text/javascript" src="js/plugins/ui/jquery.prettyPhoto.js"></script>
-<script type="text/javascript" src="js/plugins/ui/jquery.progress.js"></script>
-<script type="text/javascript" src="js/plugins/ui/jquery.timeentry.min.js"></script>
-<script type="text/javascript" src="js/plugins/ui/jquery.colorpicker.js"></script>
-<script type="text/javascript" src="js/plugins/ui/jquery.jgrowl.js"></script>
-<script type="text/javascript" src="js/plugins/ui/jquery.breadcrumbs.js"></script>
-<script type="text/javascript" src="js/plugins/ui/jquery.sourcerer.js"></script>
+<script type="text/javascript" src="plugins/ui/jquery.tipsy.js"></script>
+<script type="text/javascript" src="plugins/ui/jquery.collapsible.min.js"></script>
+<script type="text/javascript" src="plugins/ui/jquery.prettyPhoto.js"></script>
+<script type="text/javascript" src="plugins/ui/jquery.progress.js"></script>
+<script type="text/javascript" src="plugins/ui/jquery.timeentry.min.js"></script>
+<script type="text/javascript" src="plugins/ui/jquery.colorpicker.js"></script>
+<script type="text/javascript" src="plugins/ui/jquery.jgrowl.js"></script>
+<script type="text/javascript" src="plugins/ui/jquery.breadcrumbs.js"></script>
+<script type="text/javascript" src="plugins/ui/jquery.sourcerer.js"></script>
 
-<script type="text/javascript" src="js/plugins/calendar.min.js"></script>
-<script type="text/javascript" src="js/plugins/elfinder.min.js"></script>
+<script type="text/javascript" src="plugins/calendar.min.js"></script>
+<script type="text/javascript" src="plugins/elfinder.min.js"></script>
 
-<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript" src="custom.js"></script>
 
-<script type="text/javascript" src="js/charts/chart.js"></script>
+<script type="text/javascript" src="charts/chart.js"></script>
